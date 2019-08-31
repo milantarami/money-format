ee8879f3b666c469d2544f9fbe41b14b3cbc3e04
Milan Tarami <44134655+milantarami@users.noreply.github.com>
2019-08-31 19:31:11 +0545

    initial

---
+++ src/MoneyFormat.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace MilanTarami\MoneyFormat;
+
+class MoneyFormat {
+
+    private $monetarySymbols = [
+        'np' => '',
+
+    ];  
+
+    public function get($input, $countyCode, $monetarySymbol = true) {
+
+        switch(strtolower($countyCode)) {
+            case 'np':
+
+            break;
+            case 'us'
+        }
+
+    }
+
+    public function isValidInput()
+    {
+
+    }
+
+} 
